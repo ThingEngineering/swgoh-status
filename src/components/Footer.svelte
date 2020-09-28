e8c4fd83a5f6a4dc9bea316c49cc0a55015a5849
Freddie <freddie@wafflemonster.org>
2020-09-28 14:45:50 -0700

    Add some footer things and style it

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,6 +5,29 @@
     }
 </script>
 
+<style>
+    footer {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1em;
+        border-top: 1px solid #303438;
+        background: #2b2f33;
+    }
+    p {
+        margin: 0;
+        text-align: left;
+    }
+</style>
+
 <footer>
-    <a href="#" on:click={resetLocalStorage}>Reset local storage</a>
+    <div>
+        <p>
+            <a href="https://github.com/ThingEngineering/swgoh-status">GitHub project</a>
+        </p>
+        <p>SWGoH Status is not affiliated with EA, EA Capital Games, Disney, or Lucasfilm LTD.</p>
+    </div>
+    <div>
+        If things are broken: <a href="#" on:click={resetLocalStorage}>Reset local storage</a>
+    </div>
 </footer>

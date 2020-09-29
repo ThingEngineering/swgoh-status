eae0f9232215e9decf46d54ba2add6a36e497494
Freddie <freddie@wafflemonster.org>
2020-09-29 12:33:32 -0700

    Remove unneeded div wrapper

--- src/components/FarmCategory.svelte
+++ src/components/FarmCategory.svelte
@@ -9,22 +9,21 @@
 
 <style>
     section {
-        margin: 0 1rem 1rem 1rem;
-    }
-    h2 {
-        background: #2b3d4f;
-        margin: 0 0 1rem 0;
-        padding: 0.1rem 0 0.3rem 0;
-        border-top: 1px solid #3b4d5f;
-        border-bottom: 1px solid #3b4d5f;
-        font-size: 1.3rem;
-        text-align: center;
-    }
-    div {
         display: grid;
         grid-template-columns: repeat(auto-fill, 27em);
         grid-gap: 2em;
         justify-content: center;
+        margin: 0 1rem 1rem 1rem;
+    }
+    h2 {
+        background: #2b3d4f;
+        border-top: 1px solid #3b4d5f;
+        border-bottom: 1px solid #3b4d5f;
+        grid-column: 1/-1;
+        margin: 0 0 -1rem 0;
+        padding: 0.1rem 0 0.3rem 0;
+        font-size: 1.3rem;
+        text-align: center;
     }
 </style>
 
@@ -32,9 +31,7 @@
     {#if $useCategories}
         <h2>{name}</h2>
     {/if}
-    <div>
-        {#each farms as farm}
-            <Farm farm={farm} />
-        {/each}
-    </div>
+    {#each farms as farm}
+        <Farm farm={farm} />
+    {/each}
 </section>

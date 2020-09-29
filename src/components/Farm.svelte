f9082a68ab9cb96287e6e286348ff554df62b062
Freddie <freddie@wafflemonster.org>
2020-09-29 10:01:50 -0700

    Add cross/tick for farm status

--- src/components/Farm.svelte
+++ src/components/Farm.svelte
@@ -1,20 +1,38 @@
 <script>
+    import { units } from '../stores/player-data'
     import { spacer } from '../things/farm-unit';
 
     import FarmUnit from './FarmUnit.svelte'
 
     export let farm
+
+    const hasUnit = $units[farm.name] !== undefined
 </script>
 
-<style>
+<style type="text/scss">
     table {
         border: 1px solid #585b5f;
         border-collapse: collapse;
         width: 27em;
     }
+    colgroup {
+        border: 1px solid #585b5f;
+    }
     thead td {
+        position: relative;
         font-size: 1.2rem;
         text-align: center;
+
+        > span {
+            position: absolute;
+            right: 0.3rem;
+            color: #ffaaaa;
+            text-shadow: 0 0 4px rgba(0, 0, 0, 0.5);
+
+            &.has {
+                color: #aaffaa;
+            }
+        }
     }
     th {
         border: 1px solid #585b5f;
@@ -26,9 +44,6 @@
     .light {
         background: rgba(0, 116, 217, 0.5);
     }
-    colgroup {
-        border: 1px solid #585b5f;
-    }
 </style>
 
 <table>
@@ -38,7 +53,7 @@
     <colgroup span="3" class="relic"></colgroup>
     <thead>
         <tr>
-            <td colspan="11" class="{farm.side}">{farm.name}</td>
+            <td colspan="11" class="{farm.side}">{farm.name}<span class:has={hasUnit}>{hasUnit ? '✔' : '✖'}</span></td>
         </tr>
         <tr>
             <th colspan="2">Name</th>

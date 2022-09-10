008ebbb4254c044d9fcbf52c74a43f701d8afd83
dinkatoid <dinkatoid@gmail.com>
2022-09-09 20:48:11 -0500

    Added GI, Profundity, Jabba. Split out fleet

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,11 +2,13 @@ import { flatten, forEach, map } from 'lodash'
 
 import events from './events'
 import legends from './galactic-legends'
+import fleets from './fleets'
 import { FarmCategory } from '../things/farm-category'
 
 export const categories = [
     legends,
     events,
+    fleets,
 ]
 
 export const flat_categories = [

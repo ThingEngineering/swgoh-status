94cdf3daca270f0386dd984b8d0e8de3af961340
dinkatoid <dinkatoid@gmail.com>
2022-09-10 23:37:40 -0500

    added express, updated settings

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  });
+
+  app.listen(port, () =>
+  console.log(`Server is running on: http://localhost:${port}`));

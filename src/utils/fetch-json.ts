54fff4e6bdf4bfd52d811d7dea2683bf24bf9d6d
dinkatoid <dinkatoid@gmail.com>
2023-01-06 20:40:00 -0600

    more cors fun

--- src/utils/fetch-json.ts
+++ src/utils/fetch-json.ts
@@ -2,7 +2,7 @@ export default async function fetch_json(request: string): Promise<string> {
     // request = `https://cors-anywhere.swgohstatus.com/${request}`
     request = `https://proxy.cors.sh/${request}`
     let requestFull = new Request(request)
-    // requestFull.headers.append('X-Requested-With',"HelloThere")
+    requestFull.headers.append('X-Requested-With',"fetch")
     requestFull.headers.append('Origin',"https://swgohstatus.com")
     return fetch(requestFull)
         .then(response => {

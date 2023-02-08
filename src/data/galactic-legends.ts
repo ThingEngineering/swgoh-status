869a24815b67249d7adaf0892fd64b1b85acc18d
Jurek Skoczylas <Moxanity@users.noreply.github.com>
2023-02-08 11:55:46 +0000

    Adding the Jabba the Hutt GL event

--- src/data/galactic-legends.ts
+++ src/data/galactic-legends.ts
@@ -6,6 +6,30 @@ export default new FarmCategory(
     'Galactic Legends',
     [
         new Farm(
+            'Jabba the Hutt',
+            'legend',
+            'dark',
+            [
+                new FarmUnit('Krrsantan', 7, 13, 5),
+                new FarmUnit('Han Solo', 7, 13, 8),
+                new FarmUnit('Gamorrean Guard', 7, 13, 3),
+                new FarmUnit('Greedo', 7, 13, 6),
+                new FarmUnit('Skiff Guard (Lando Calrissian)', 7, 13, 5),
+                new FarmUnit('Jedi Knight Luke Skywalker', 7, 13, 7),
+                new FarmUnit('Jawa', 7, 13, 3),
+                new FarmUnit('URoRRuR\'R\'R', 7, 13, 4),
+                new FarmUnit('C-3PO', 7, 13, 7),
+                new FarmUnit('Boushh (Leia Organa)', 7, 13, 5),
+                new FarmUnit('Aurra Sing', 7, 13, 6),
+                new FarmUnit('Fennec Shand', 7, 13, 7),
+                new FarmUnit('Boba Fett', 7, 13, 7),
+                new FarmUnit('Mob Enforcer', 7, 13, 3),
+            ],
+            [
+                new FarmUnit('Outrider', 7),
+            ],
+        ),
+        new Farm(
             'Lord Vader',
             'legend',
             'dark',
